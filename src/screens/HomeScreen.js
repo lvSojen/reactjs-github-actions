919f3b793fab9df8522ecf9312594f275d64f2ea
Yao Wang <ywang011@odu.edu>
2023-01-09 09:04:44 -0500

    update

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap"
 function HomeScreen() {
   return (
     <div>
-      <div>HomeScreen</div>
+      <div>411W GOLD HomeScreen</div>
       <Link to="/product">
         <Button variant="primary">Go to Product Screen</Button>
       </Link>
